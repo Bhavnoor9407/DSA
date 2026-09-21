d4e03ca8608ff516d049935d48643cc0ef0d03a6
Bhavnoor Singh <ytzindabad08@gmail.com>
2026-09-21 23:26:12 +0530

    Time: 4 ms (65.92%) | Memory: 11.2 MB (71.73%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int val[256] = {};
+        
+        val['I'] = 1;
+        val['V'] = 5;
+        val['X'] = 10;
+        val['L'] = 50;
+        val['C'] = 100;
+        val['D'] = 500;
+        val['M'] = 1000;
+
+        int ans = 0;
+
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (val[s[i]] < val[s[i + 1]])
+                ans -= val[s[i]];
+            else
+                ans += val[s[i]];
+        }
+
+        ans += val[s.back()];
+
+        return ans;
+    }
+};
